d978430db597c35bdddbe5cf922a67dd08ae0aaa
Ericmr8 <148563364+Ericmr8@users.noreply.github.com>
2025-05-06 13:21:09 +0200

    Add files via upload

---
+++ M3Practica11EMR_Excepciones/src/exceptions/CheckDNIException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class CheckDNIException extends Exception {
+
+    public CheckDNIException(String message) {
+        super(message);
+    }
+}
---
+++ M3Practica11EMR_Excepciones/src/exceptions/CheckEmailException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class CheckEmailException extends Exception {
+
+    public CheckEmailException(String message) {
+        super(message);
+    }
+}
